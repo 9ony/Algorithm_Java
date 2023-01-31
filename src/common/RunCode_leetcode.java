499c699893dfbe96ca62647911f021b4fa78835e
9ony <darsta7@gmail.com>
2023-01-31 19:00:29 +0900

    leetcode 1번문제

---
+++ src/common/RunCode_leetcode.java
@@ -0,0 +1,17 @@
+package common;
+
+import java.util.Arrays;
+
+import leetcode.Two_Sum;
+
+public class RunCode_leetcode {
+
+	public static void main(String[] args) {
+		// 1.Tow sum    (Easy)
+		
+		Two_Sum app = new Two_Sum();
+		int[] nums = {2,3,4};
+		System.out.println(Arrays.toString(app.twoSum(nums, 6)));
+		
+	}
+}
