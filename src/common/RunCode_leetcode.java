8a4a8eae355bb833fdae1d0f2dc4ec2eb4967d4c
9ony <darsta7@gmail.com>
2023-02-02 20:11:10 +0900

    leetcode 3번문제

--- src/common/RunCode_leetcode.java
+++ src/common/RunCode_leetcode.java
@@ -1,6 +1,6 @@
 package common;
 
-import leetcode.Add_Two_Numbers;
+import leetcode.Longest_Substring_Without_Repeating_Characters;
 
 public class RunCode_leetcode {
 
@@ -12,7 +12,7 @@ public class RunCode_leetcode {
 		System.out.println(Arrays.toString(app.twoSum(nums, 6)));
 		*/
 		
-		
+		/*
 		// 2.Add Two Numbers  (Medium)
 		Add_Two_Numbers app = new Add_Two_Numbers();
 		ListNode l1 = new ListNode(4);
@@ -25,6 +25,14 @@ public class RunCode_leetcode {
 		ListNode result = app.addTwoNumbers(l1, l2);
 		
 		System.out.println(result.toStringListNode());
+		*/
 		
+		
+		// 3. Longest Substring Without Repeating Characters  (Medium)
+		Longest_Substring_Without_Repeating_Characters app = new Longest_Substring_Without_Repeating_Characters();
+		// app.lengthOfLongestSubstring 시간복잡도초과로인한 실패
+		//System.out.println(app.lengthOfLongestSubstring("abcabcaa"));
+		System.out.println(app.lengthOfLongestSubstring2("ababcdc"));
+
 	}
 }
